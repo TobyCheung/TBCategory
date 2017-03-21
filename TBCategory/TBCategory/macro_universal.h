00ad61a56757e20cb84d5ccbcf57c8eac254195f
TobyCheung <ztb1128@gmail.com>
2017-03-21 17:50:12 +0800

    宏文件

---
+++ TBCategory/TBCategory/macro_universal.h
@@ -0,0 +1,16 @@
+//
+//  macro_universal.h
+//  TBCategory
+//
+//  Created by ZhangTianbing on 2017/3/21.
+//  Copyright © 2017年 张天兵. All rights reserved.
+//
+
+#ifndef macro_universal_h
+#define macro_universal_h
+
+
+
+
+
+#endif /* macro_universal_h */
