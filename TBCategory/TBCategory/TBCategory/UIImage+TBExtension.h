f726863956e9724f92ba660d5384b51e2f7c432c
TobyCheung <ztb1128@gmail.com>
2017-04-26 10:19:17 +0800

    编辑注释

--- TBCategory/TBCategory/TBCategory/UIImage+TBExtension.h
+++ TBCategory/TBCategory/TBCategory/UIImage+TBExtension.h
@@ -10,36 +10,64 @@
 
 @interface UIImage (TBExtension)
 
-/** 把方形图片裁剪为圆形 */
+/** 
+ 把方形图片裁剪为圆形 
+ */
 - (instancetype)tb_circleImage;
-/**  把方形图片裁剪为圆形 */
+
+/** 
+ 把方形图片裁剪为圆形 
+ */
 + (instancetype)tb_circleImage:(NSString *)name;
 
-/** 把方形图片裁剪为圆角矩形 */
+/** 
+ 把方形图片裁剪为圆角矩形 
+ */
 - (instancetype)tb_roundedRectImageWithCornerRadius:(CGFloat)cornerRadius;
-/** 把方形图片裁剪为圆角矩形 */
+
+/** 
+ 把方形图片裁剪为圆角矩形
+ */
 + (instancetype)tb_roundedRectImage:(NSString *)name cornerRadius:(CGFloat)cornerRadius;
 
-/** 给定一个图片名称生成一个不会被渲染的图片 */
+/** 
+ 给定一个图片名称生成一个不会被渲染的图片
+ */
 + (instancetype)tb_imageWithOriginalImageNamed:(NSString *)imageName;
 
-/** 从给定view生成一张图片 */
+/** 
+ 从给定view生成一张图片 
+ */
 + (UIImage *)tb_getImageFromView:(UIView *)view;
 
-/** 合成两张图片 */
+/** 
+ 合成两张图片 
+ */
 + (UIImage *)tb_coverImage:(UIImage *)image1 withImage:(UIImage *)image2;
 
-/** 为给定图片生成一张带蒙版的圆角矩形图片 */
+/** 
+ 为给定图片生成一张带蒙版的圆角矩形图片
+ */
 - (instancetype)tb_roundedRectImageWithCornerRadius:(CGFloat)cornerRadius coverAlpha:(CGFloat)alpha;
 
-/** 为image生成一张带边框的图片 */
+/** 
+ 为image生成一张带边框的图片
+ */
 + (UIImage *)tb_bolderImageWithImage:(UIImage *)image imageSize:(CGSize)size borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
-/** 生成一张带边框的图片 */
+
+/** 
+ 生成一张带边框的图片
+ */
 - (UIImage *)tb_getBolderImageWithBorderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
 
-/** 缩放图片至给定尺寸 */
+/** 
+ 缩放图片至给定尺寸
+ */
 + (UIImage *)tb_originImage:(UIImage *)image scaleToSize:(CGSize)size;
-/** 缩放至给定尺寸 */
+
+/** 
+ 缩放至给定尺寸
+ */
 - (UIImage *)tb_scaleToSize:(CGSize)size;
 
 @end
